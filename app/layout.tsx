0d31120fe688179e7b24979b6174064e57a01891
Pastor Mauricio López <pentecostalesnicas@gmail.com>
2026-07-22 09:29:07 -0600

    Sprint 2 - Arquitectura y shadcn instalados

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Conectando tu Vida a Dios",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" className={cn("font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   );
